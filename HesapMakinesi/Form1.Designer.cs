4014f06f38fe475eb310011d743d29f4f04a77db
ralpd <ralpd@LAPTOP-FB9086OP>
2023-09-21 01:14:38 +0300

    Add project files.

---
+++ HesapMakinesi/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace HesapMakinesi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtValue = new System.Windows.Forms.TextBox();
+            this.btnPlus = new System.Windows.Forms.Button();
+            this.btnMinus = new System.Windows.Forms.Button();
+            this.btnMultiple = new System.Windows.Forms.Button();
+            this.btnDividedBy = new System.Windows.Forms.Button();
+            this.btnEquals = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtValue
+            // 
+            this.txtValue.Location = new System.Drawing.Point(240, 81);
+            this.txtValue.Name = "txtValue";
+            this.txtValue.Size = new System.Drawing.Size(220, 22);
+            this.txtValue.TabIndex = 0;
+            // 
+            // btnPlus
+            // 
+            this.btnPlus.Location = new System.Drawing.Point(240, 123);
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.Size = new System.Drawing.Size(95, 49);
+            this.btnPlus.TabIndex = 1;
+            this.btnPlus.Text = "+";
+            this.btnPlus.UseVisualStyleBackColor = true;
+            this.btnPlus.Click += new System.EventHandler(this.btnPlus_Click);
+            // 
+            // btnMinus
+            // 
+            this.btnMinus.Location = new System.Drawing.Point(365, 123);
+            this.btnMinus.Name = "btnMinus";
+            this.btnMinus.Size = new System.Drawing.Size(95, 49);
+            this.btnMinus.TabIndex = 2;
+            this.btnMinus.Text = "-";
+            this.btnMinus.UseVisualStyleBackColor = true;
+            this.btnMinus.Click += new System.EventHandler(this.btnMinus_Click);
+            // 
+            // btnMultiple
+            // 
+            this.btnMultiple.Location = new System.Drawing.Point(240, 201);
+            this.btnMultiple.Name = "btnMultiple";
+            this.btnMultiple.Size = new System.Drawing.Size(95, 49);
+            this.btnMultiple.TabIndex = 3;
+            this.btnMultiple.Text = "*";
+            this.btnMultiple.UseVisualStyleBackColor = true;
+            this.btnMultiple.Click += new System.EventHandler(this.btnMultiple_Click);
+            // 
+            // btnDividedBy
+            // 
+            this.btnDividedBy.Location = new System.Drawing.Point(365, 201);
+            this.btnDividedBy.Name = "btnDividedBy";
+            this.btnDividedBy.Size = new System.Drawing.Size(95, 49);
+            this.btnDividedBy.TabIndex = 4;
+            this.btnDividedBy.Text = "/";
+            this.btnDividedBy.UseVisualStyleBackColor = true;
+            this.btnDividedBy.Click += new System.EventHandler(this.btnDividedBy_Click);
+            // 
+            // btnEquals
+            // 
+            this.btnEquals.Location = new System.Drawing.Point(500, 160);
+            this.btnEquals.Name = "btnEquals";
+            this.btnEquals.Size = new System.Drawing.Size(95, 49);
+            this.btnEquals.TabIndex = 5;
+            this.btnEquals.Text = "=";
+            this.btnEquals.UseVisualStyleBackColor = true;
+            this.btnEquals.Click += new System.EventHandler(this.btnEquals_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnEquals);
+            this.Controls.Add(this.btnDividedBy);
+            this.Controls.Add(this.btnMultiple);
+            this.Controls.Add(this.btnMinus);
+            this.Controls.Add(this.btnPlus);
+            this.Controls.Add(this.txtValue);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtValue;
+        private System.Windows.Forms.Button btnPlus;
+        private System.Windows.Forms.Button btnMinus;
+        private System.Windows.Forms.Button btnMultiple;
+        private System.Windows.Forms.Button btnDividedBy;
+        private System.Windows.Forms.Button btnEquals;
+    }
+}
+
---
+++ HesapMakinesi/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HesapMakinesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double number1;
+        string op;
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            number1 = Convert.ToDouble(txtValue.Text);
+            op = "+";
+
+        }
+
+        private void btnMultiple_Click(object sender, EventArgs e)
+        {
+            number1 = Convert.ToDouble(txtValue.Text);
+            op = "*";
+        }
+
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+            number1 = Convert.ToDouble(txtValue.Text);
+            op = "-";
+        }
+
+        private void btnDividedBy_Click(object sender, EventArgs e)
+        {
+            number1 = Convert.ToDouble(txtValue.Text);
+            op = "/";
+        }
+
+        private void btnEquals_Click(object sender, EventArgs e)
+        {
+            double number2 = Convert.ToDouble(txtValue.Text);
+            double result=0;
+            number2 = Convert.ToDouble(txtValue.Text);
+            if( op == "+")
+            {
+                result = number1 + number2;
+            }
+            else if (op == "-")
+            {
+                result = number1 - number2;
+            }
+            else if (op == "*")
+            {
+                result = number1 * number2;
+            }
+            else if (op == "/")
+            {
+                result = number1 / number2;
+            }
+            txtValue.Text = result.ToString();
+        }
+    }
+    }
+    
+
